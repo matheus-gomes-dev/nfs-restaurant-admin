22ce8c1d2fbdd4ed1ef768134c8b3f7ad60cc7b9
matheus-gomes-ispot <matheus.delimagomes@ispot.tv>
2026-01-08 21:07:48 -0300

    AddOrderModal styling enhancement for mobile

--- app/components/AddOrderModal.tsx
+++ app/components/AddOrderModal.tsx
@@ -61,6 +61,7 @@ export default function AddOrderModal({ open, onClose, onAddOrder, products }: A
         left: '50%',
         transform: 'translate(-50%, -50%)',
         width: 600,
+        maxWidth: '90vw',
         bgcolor: 'background.paper',
         boxShadow: 24,
         p: 4,
